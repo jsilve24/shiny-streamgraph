78d01447a7be62db54fdd11eac4836ab05da0602
Justin Silverman <jsilve24@gmail.com>
2016-06-18 13:20:44 -0400

    Added Test Data
    Also Removed personal setwd call.
    
    Signed-off-by: Justin Silverman <jsilve24@gmail.com>

--- testing_streamgraph.R
+++ testing_streamgraph.R
@@ -4,23 +4,20 @@ library(babynames)
 library(dplyr)
 library(tidyr)
 
-setwd('~/Research/streamgraph/')
-
 data.wide <- babynames %>%
   filter(grepl("^Kr", name)) %>%
   group_by(year, name) %>%
   tally(wt=n) %>% 
   spread(name, n)
 data.wide[is.na(data.wide)] <- 0 
-write.csv(data.wide, file='babynames.kr.wide.csv', row.names = F)
+
 
 
 data.long <- babynames %>% 
   filter(grepl("^Kr", name)) %>%
   group_by(year, name) %>%
   tally(wt=n)
-write.csv(data.long, file='babynames.kr.long.csv', row.names=F)
-    
+
 sg <- babynames %>%
   filter(grepl("^Kr", name)) %>%
   group_by(year, name) %>%
